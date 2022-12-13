7a8555a2bf2eb1ff77f58640ed18822a0b7d822b
Teshi Waruingi <71100831+teshiwaruingi@users.noreply.github.com>
2022-12-13 18:21:15 -0500

    Update process.c

--- process.c
+++ process.c
@@ -3,44 +3,50 @@
 #include <unistd.h>
 #include <time.h>
 
+// generates a random number between the given lower and upper bounds
 int genRandonNumber(int lower, int upper) {
- return ((random() % (upper - lower)) + lower);
+  return ((random() % (upper - lower)) + lower);
 }
 
+// the code that the child process should execute
 void ChildProcess() {
   int i;
   int max_count = genRandonNumber(0, 30);
+
+  // the child process goes to sleep and wakes up a random number of times
   for (i = 1; i <= max_count; i++) {
-     printf("Child %d is going to sleep!\n", getpid());
-     sleep(genRandonNumber(0,10));
-     printf("Child %d is awake!\n Where is my parent: %d? \n",getpid(),getppid());
-}
+    printf("Child %d is going to sleep!\n", getpid());
+    sleep(genRandonNumber(0, 10));
+    printf("Child %d is awake!\n Where is my parent: %d? \n", getpid(), getppid());
+  }
+
+  // exit the child process
   exit(0);
-};
+}
 
 int main(int argc, char * argv[]) {
-  srandom(time(0));
-  pid_t  pid;
-   int    i; 
-   pid = fork();
-   if (pid == 0) {
-        ChildProcess();
-   }
-   else if (pid == -1) {
-     exit(1);
-   }
-   else {
-     pid = fork();
-     if (pid == 0) {
-       ChildProcess();
-       } 
-     else if (pid == -1) {
-       exit(1);
-     }
-     else {
-       for (i=0; i<2; i++){
-       pid = wait(NULL);
-       printf("Child %d has completed\n", pid);}
-     }
-   }
-}
+  srandom(time(0)); // seed the random number generator
+
+  pid_t pid;
+  int i;
+
+  // create the first child process
+  pid = fork();
+  if (pid == 0) {
+    ChildProcess();
+  } else if (pid == -1) {
+    exit(1);
+  } else {
+    // create the second child process
+    pid = fork();
+    if (pid == 0) {
+      ChildProcess();
+    } else if (pid == -1) {
+      exit(1);
+    } else {
+      // wait for both child processes to finish
+      for (i = 0; i < 2; i++) {
+        pid = wait(NULL);
+        printf("Child %d has completed\n", pid);
+      }
+    }
